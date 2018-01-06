6cc1d34eaa402c5d863dee8600c54d8a7cd9b5cd
Michael Chacaton <chako256@gmail.com>
2018-01-06 02:02:35 +0100

    Refactor to ES7

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict'
 
-const path = require('path')
-const co = require('co')
-const pify = require('promise.ify')
-const fs = pify.all(require('fs-extra'))
+const Path = require('path')
+const Fs = require('fs')
+const Bluebird = require('bluebird')
 const _ = require('lodash')
 const parser = require('./lib/parser.js')
 const LocalUtil = require('./lib/util.js')
+
+const openAsync = Bluebird.promisify(Fs.open)
+const fstatAsync = Bluebird.promisify(Fs.fstat)
+const readAsync = Bluebird.promisify(Fs.read)
 
 const FILE_TOO_SMALL = 'file too small'
 
@@ -14,56 +17,47 @@ const FILE_TOO_SMALL = 'file too small'
  * get ID3 info
  */
 
-module.exports = co.wrap(function*(fd) {
+module.exports = async (fd) => {
     if (typeof fd !== 'number') {
-        const filename = path.resolve(fd)
-        fd = yield fs.openAsync(filename, 'r')
+        const filename = Path.resolve(fd)
+        fd = await openAsync(filename, 'r')
     }
-
-    const stat = yield fs.fstatAsync(fd)
+    const stat = fstatAsync(fd)
     if (stat.size < 10) {
         throw new Error(FILE_TOO_SMALL)
     }
 
     const buf = Buffer.alloc(4)
-    yield fs.readAsync(
-        fd,
-        buf,
-        0,
-        buf.length,
-        6 // offset, length, position
-    )
+    await readAsync(fd, buf, 0, buf.length, 6)
+
     const minSize = LocalUtil.getID3TotalSize(buf)
+    console.log(`ID3 Size : ${minSize} bytes`)
     if (stat.size <= minSize) {
         throw new Error(FILE_TOO_SMALL)
     }
 
-    const stream = fs.createReadStream('', {
-        fd,
-    })
+    const stream = Fs.createReadStream('', { fd })
     const p = parser.create()
     process.nextTick(() => {
         stream.pipe(p)
     })
 
-    const info = yield new Promise((resolve) => {
-        p.on('readable', function() {
+    const info = await new Promise((resolve) => {
+        p.on('readable', () => {
             resolve(p.read())
         })
     })
-
-    const ret = {}
 
     let singer = _.find(info.ID3, ['id', 'TPE1'])
     let title = _.find(info.ID3, ['id', 'TIT2'])
     let album = _.find(info.ID3, ['id', 'TALB'])
     let genre = _.find(info.ID3, ['id', 'TCON'])
     let year = _.find(info.ID3, ['id', 'TYER'])
-    singer = singer.content
-    title = title.content
-    album = album.content
-    genre = genre.content
-    year = year.content
+    singer = singer ? singer.content : null
+    title = title ? title.content : null
+    album = album ? album.content : null
+    genre = genre ? genre.content : null
+    year = year ? year.content : null
 
     return {
         singer,
@@ -73,4 +67,4 @@ module.exports = co.wrap(function*(fd) {
         year,
         raw : info,
     }
-})
+}
